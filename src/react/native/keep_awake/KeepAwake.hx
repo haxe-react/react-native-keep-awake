9cf7a00554aba50aa3559bcea343df23cc148978
Kevin Leung <kevinresol@gmail.com>
2019-03-25 21:36:37 +0800

    Fix

--- src/react/native/keep_awake/KeepAwake.hx
+++ src/react/native/keep_awake/KeepAwake.hx
@@ -2,5 +2,5 @@ package react.native.keep_awake;
 
 import react.ReactComponent;
 
-@:jsRequire('react-native-keep-awake')
+@:jsRequire('react-native-keep-awake', 'default')
 extern class KeepAwake extends ReactComponentOfProps<{}> {}
